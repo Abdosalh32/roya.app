ff5f9a278b68c8be276c496e1e49808506eeaa69
Jehad <jeha710@gmail.com>
2026-04-08 13:19:13 +0200

    edit the tab bar

--- lib/features/dashboard/views/main_shell.dart
+++ lib/features/dashboard/views/main_shell.dart
@@ -29,7 +29,7 @@ class MainShell extends StatelessWidget {
         child: BottomNavigationBar(
           currentIndex: navigationShell.currentIndex,
           onTap: _onTap,
-          backgroundColor: Colors.transparent,
+          backgroundColor: Colors.white,
           type: BottomNavigationBarType.fixed,
           selectedItemColor: AppColors.primary,
           unselectedItemColor: AppColors.textSecondary,
